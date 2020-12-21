d628e9fee4d2f6a3f6d2e49cd1a397dd51cdba05
AnigalHub <anigal1997@gmail.com>
2020-12-21 22:12:08 +0300

    добавлены все компоненты по сайту

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@
       <Navbar/>
       <About/>
       <Skills/>
+      <Works/>
+      <Reviews/>
+      <Contacts/>
+      <Myfooter/>
     </div>
 </template>
 
@@ -12,8 +16,12 @@ import Home from "~/components/home";
 import Navbar from "~/components/navbar";
 import About from "~/components/about";
 import Skills from "~/components/skills";
+import Works from "~/components/works";
+import Reviews from "~/components/reviews";
+import Contacts from "~/components/contacts";
+import Myfooter from "~/components/myfooter";
 export default {
-  components: {Skills, About, Navbar, Home}
+  components: {Myfooter, Contacts, Reviews, Works, Skills, About, Navbar, Home}
 }
 </script>
 
@@ -21,15 +29,63 @@ export default {
 .container {
   padding: 15px !important;
 }
-h2{
-  border-bottom: 2px solid white;
+@media screen and (prefers-color-scheme: light){
+
+  #About, #Works,#Contacts{
+    background: white;
+    color: #090c13 !important;
+    padding: 3rem 0 !important;
+
+  }
+  #Skills,#Reviews{
+    background: rgba(208, 204, 203, 0.61);
+    color: #090c13 !important;
+    padding: 3rem 0 !important;
+  }
+  .carousel-caption{
+    color: #090c13 !important;
+  }
+
+  h2 {
+    border-bottom: 2px solid  #090c13 ;
+    box-shadow: 0 8px 8px -8px  #090c13 ;
+  }
+  .svg_color{
+    fill: #090c13;
+  }
+}
+@media screen and (prefers-color-scheme: dark){
+
+  #About, #Works,#Contacts{
+    background: #090c13;
+    color: white !important;
+    padding: 3rem 0 !important;
+
+  }
+  #Skills,#Reviews{
+    background: #0c0f17;
+    color: white !important;
+    padding: 3rem 0 !important;
+  }
+  h2 {
+    border-bottom: 2px solid white;
+    box-shadow: 0 8px 6px -6px rgba(255, 255, 255, 0.63);
+  }
+  .svg_color{
+    fill:#ffffff;
+  }
+}
+#Nav,#Myfooter{
+  background: #030407;
   box-shadow: 0 8px 6px -6px rgba(255, 255, 255, 0.63);
+}
+h2{
   font-family: 'Piazzolla', serif;
   margin-left: auto;
   margin-right: auto;
   text-align: center;
   font-weight: 800;
-  padding-bottom: 1% !important;
+  padding-bottom: 2% !important;
 }
 h3{
   font-family: 'Piazzolla', serif;
@@ -40,20 +96,18 @@ h3{
   font-family: 'Piazzolla', serif;
   font-size: 1.3rem !important;
 }
-#Nav{
-  background: #252c3d;
-  box-shadow: 0 8px 6px -6px rgba(255, 255, 255, 0.63);
-}
-#About, #Skils{
-  background: rgba(43, 52, 69, 0.9);
-  color: white !important;
-  padding: 3rem 0 !important;
-}
-#About h2{
+
+#About h2, #Contacts h2{
   width: 6em;
 }
-#Skils h2{
+#Skills h2{
   width: 10em;
+}
+#Works h2{
+  width: 8em;
+}
+#Reviews h2{
+  width: 4em;
 }
 .title {
   font-family:
