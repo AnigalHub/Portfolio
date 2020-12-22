7e01fac0451cea5f89dc975b164226a4e28088cd
AnigalHub <anigal1997@gmail.com>
2020-12-22 03:57:37 +0300

    компонент - footer

---
+++ components/myfooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="Myfooter">
+      <b-container class="text">
+        Copyright © 2020 <br>
+        <Logo_svg/>
+        Ignatova Anna
+      </b-container>
+    </div>
+</template>
+
+<script>
+    import Logo_svg from "~/components/logo_svg";
+    export default {
+        name: "myfooter",
+      components: {Logo_svg}
+    }
+</script>
+
+<style scoped>
+
+</style>
