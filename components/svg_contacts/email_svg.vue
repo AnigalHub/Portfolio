8c50ea4f6ab2594bc1194fc9c3fec9e226cb9d8f
AnigalHub <anigal1997@gmail.com>
2020-12-23 23:49:44 +0300

    svg по контактам - contacts

---
+++ components/svg_contacts/email_svg.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 172 172" style=" fill:#000000;">
+    <g fill="none" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+      <path d="M0,172v-172h172v172z" fill="none"></path>
+      <g class="svg_color">
+        <path d="M14.54609,34.4l64.63438,60.32317c3.82413,3.5776 9.82066,3.5776 13.63906,0l64.63438,-60.32317zM5.73333,41.85781v87.6349l45.32917,-45.32917zM166.26667,41.85781l-45.32917,42.30573l45.32917,45.32917zM59.43854,91.99089l-45.59792,45.60911h144.31875l-45.60912,-45.60911l-11.90338,11.10833c-4.1108,3.8356 -9.38914,5.75573 -14.65758,5.75573c-5.26893,0 -10.53608,-1.92013 -14.64688,-5.75573z"></path>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+    export default {
+        name: "email_svg"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/svg_contacts/telegram_svg.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"  viewBox="0 0 172 172" style=" fill:#000000;">
+    <g fill="none" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+      <path d="M0,172v-172h172v172z" fill="none"></path>
+      <g class="svg_color">
+        <path d="M158.71128,22.53888c-2.58,-2.18784 -6.63232,-2.50088 -10.82224,-0.81872h-0.00688c-4.40664,1.76816 -124.73784,53.38192 -129.6364,55.49064c-0.89096,0.3096 -8.67224,3.21296 -7.87572,9.67516c0.71552,5.8308 6.96944,8.24568 7.73312,8.52432l30.59192,10.4748c2.0296,6.75616 9.5116,31.6824 11.16624,37.00752c1.032,3.3196 2.71416,7.68152 5.66224,8.57936c2.58688,0.9976 5.16,0.086 6.82496,-1.2212l18.75328,-17.34792l30.19288,23.5468l0.71896,0.43c2.05024,0.90816 4.01448,1.36224 5.88928,1.36224c1.44824,0 2.838,-0.27176 4.16584,-0.81528c4.5236,-1.8576 6.33304,-6.16792 6.52224,-6.6564l22.55264,-117.22488c1.376,-6.2608 -0.53664,-9.44624 -2.38736,-11.01144zM75.68,110.08l-10.32,27.52l-10.32,-34.4l79.12,-58.48z"></path>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+    export default {
+        name: "telegram_svg"
+    }
+</script>
+
+<style scoped>
+
+</style>
