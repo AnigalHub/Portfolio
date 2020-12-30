870bbae297cfac53139a26a7c0ec5a4d8adc2f05
AnigalHub <anigal1997@gmail.com>
2020-12-30 04:42:02 +0300

    добавлено общее svg, удален массив с тремя svg

--- components/skills.vue
+++ components/skills.vue
@@ -2,11 +2,11 @@
     <div id="Skills">
       <b-container>
       <h2>Ключевые навыки</h2>
-        <b-row>
+        <b-row class="important_row">
           <b-col v-for="(skill,index) in SkillsRow" :key="index">
             <b-row v-for="language in skill.programming_languages" :key="language.descr_skills">
               <b-col cols="3">
-                <component v-for="(value, index) in language.svgs" :key="index" :is="value.svg"/>
+                <component :key="index" :is="language.svg"/>
               </b-col>
               <b-col class="text">
                 {{language.descr_skills}}
@@ -27,33 +27,31 @@
 </template>
 
 <script>
-    import Html_svg from "~/components/svg_skills/html_svg";
-    import Css_svg from "~/components/svg_skills/css_svg";
-    import Bootstrap_svg from "~/components/svg_skills/bootstrap_svg";
     import Vue_svg from "~/components/svg_skills/vue_svg";
     import Javascript_svg from "~/components/svg_skills/javascript_svg";
     import Sass_svg from "~/components/svg_skills/sass_svg";
     import Nuxt_svg from "~/components/svg_skills/nuxt_svg";
     import Flexbox_svg from "~/components/svg_skills/flexbox_svg";
     import Git_svg from "~/components/svg_skills/git_svg";
+    import Html_css from "~/components/svg_skills/html_css_bootstrap";
     export default {
         name: "skills",
-      components: {Git_svg, Flexbox_svg, Nuxt_svg, Sass_svg, Javascript_svg, Vue_svg, Bootstrap_svg, Css_svg, Html_svg},
+      components: {Git_svg, Flexbox_svg, Nuxt_svg, Sass_svg, Javascript_svg, Vue_svg, Html_css},
       data(){
           return{
             SkillsRow:[
               {
                 programming_languages:[
                   {
-                    svgs:[{svg:Html_svg,}, {svg:Css_svg,}, {svg:Bootstrap_svg,}],
-                    descr_skills:'Уверенные знания HTML5,CSS,BOOTSTRAP',
+                    svg:Html_css,
+                    descr_skills:'Уверенные знания HTML5,CSS3,BOOTSTRAP',
                   },
                   {
-                    svgs: [{svg: Vue_svg,}],
+                    svg: Vue_svg,
                     descr_skills: 'Опыт работы с фреймворком VUE.JS',
                   },
                   {
-                    svgs: [{svg: Javascript_svg,}],
+                    svg:Javascript_svg,
                     descr_skills: 'Базовые знания JAVASCRIPT',
                   }
                 ]
@@ -61,15 +59,15 @@
               {
                 programming_languages:[
                   {
-                    svgs:[{svg:Sass_svg,},],
+                    svg:Sass_svg,
                     descr_skills:'Работа с препроцессором SASS/SCSS',
                   },
                   {
-                    svgs: [{svg: Nuxt_svg,}],
+                    svg: Nuxt_svg,
                     descr_skills: 'Умение работать с фреймворком NUXT.JS',
                   },
                   {
-                    svgs: [{svg: Flexbox_svg,}],
+                    svg: Flexbox_svg,
                     descr_skills: 'Практический опыт адаптивной кроссбраузерной верски (в том числе flexbox и grid)',
                   }
                 ]
