274387e8197ad42114ebdc3a2a87a8e20026cefc
AnigalHub <anigal1997@gmail.com>
2020-12-30 04:54:00 +0300

    добавлено описание компаний, а также использованные технологии в каждом сайте и адрес

--- components/works.vue
+++ components/works.vue
@@ -5,14 +5,16 @@
         <div class="flex-container">
          <div v-for="(work,index) in WorksRow" :key="index">
            <b-row>
-             <b-col cols="6">
+             <b-col cols="4">
                <img :src="work.src" :alt="work.alt"/>
              </b-col>
              <b-col>
-               cc
+              <h4>{{work.name_company}}</h4>
+               <p class="text_works">{{work.descr_company}}</p>
+               <p class="text_works"><u><b>Использованные технологии:</b></u><br>{{work.tecnology}}</p>
+               <p class="text_works"><u><b>Адрес сайта:</b></u><a :href="work.url" target="_blank">{{work.website}}</a></p>
              </b-col>
            </b-row>
-
          </div>
         </div>
       </b-container>
@@ -28,22 +30,49 @@
               {
                 src:'./home_prom.jpg',
                 alt:'Промэлектронсервис',
+                name_company:'Сайт для компании «Промэлектронсервис»',
+                descr_company:'Компания занимается сертифационными испытаниями и поставкой электронных компонентов.',
+                tecnology:'Адаптивная вёрстка на Bootstrap 4, HTML5, CSS3, форма обратной связи на PHP.',
+                url:'http://promelektronservis.ru/',
+                website:'promelektronservis.ru',
               },
               {
                 src:'./home_prom2.png',
                 alt:'Промэлектронсервис_2',
+                name_company:'Сайт для компании «Промэлектронсервис»',
+                descr_company:'Компания занимается сертифационными испытаниями и поставкой электронных компонентов.',
+                tecnology:'Второй релиз сайта - переписанный на Vue.js (с использованием Vue router)',
+                url:'',
+                website:'(сайт не был запущен)',
               },
               {
                 src:'./home_gts.png',
                 alt:'Гостехносервис',
+                name_company:'Сайт для компании «Гостехносервис»',
+                descr_company:'Компания поставляет IT-оборудование, производит аксессуары и вычислительных комплексы',
+                tecnology:'Одностраничный сайт на Vue.js (Sass, BootstrapVue) - с простой анимацией для svg-элементов ',
+                url:'https://gtekhno.ru/',
+                website:'gtekhno.ru',
               },
               {
                 src:'./home_priamyeruki.jpg',
                 alt:'Прямые_руки',
+                name_company:'Сайт для компании «Аваконт»',
+                descr_company:'Компания оказывает услуги по изготовлению разнообразного декора, рекламных вывесок,\n' +
+                  'сувенирной продукции, реставрация, брендирование.',
+                tecnology:'FIGMA (дизайн-макет сайта), HTML5, CSS3, Vue.js (Sass, BootstrapVue, Vue router), обратная связь на Javascript',
+                url:'',
+                website:'priamyeruki.ru',
               },
               {
                 src:'./home_avacont.jpg',
                 alt:'Аваконт',
+                name_company:'Сайт для компании «Аваконт»',
+                descr_company:'Компания занимается изготовлением накладок из дерева под розетки,\n' +
+                  ' выключатели, радиаторы, светильники и карнизы разных производителей.',
+                tecnology:'Создание макета сайта с помощью FIGMA. Разработка лендинга на Vue.js, HTML5, SASS, форма обратной связи и подсчет стоимости товара на JavaScript',
+                url:'',
+                website:'avacont.ru',
               }
             ]
           }
@@ -59,17 +88,28 @@
   }
   .flex-container > div {
     background-color: rgba(255, 255, 255, 0.8);
-    box-shadow: 5px 5px 5px rgb(24, 31, 49);
+    box-shadow: 7px 4px 11px -1px rgb(24, 31, 49);
     text-align: center;
+    width: 100%;
+    margin: 0 1.2% 2.5% 1.2% !important;
+  }
+  img{
+    width: 100%;
+    height: 100%;
   }
   .flex-container > div:hover{
-    box-shadow: 8px 10px 10px rgb(24, 31, 49);
+    box-shadow: 7px 4px 16px -1px rgb(24, 31, 49);
   }
-  .col-6{
+  .col-4{
     padding: 0 !important;
   }
   .row{
     margin: 0 !important;
   }
-
+  .col{
+    padding: 15px !important;
+  }
+  p{
+    margin-bottom: 0.5rem !important;
+  }
 </style>
