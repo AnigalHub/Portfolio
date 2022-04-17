51cc8fb174caa8270664ff35fe675dbcc8795a99
AnigalHub <anigal1997@gmail.com>
2022-04-17 22:54:52 +0300

    svg на css

---
+++ components/svg_skills/css_svg.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+    <path d="m41 5h-34l3 34 14 4 14-4 3-34z" fill="#0277BD"/>
+    <path d="m24 8v31.9l11.2-3.2 2.5-28.7z" fill="#039BE5"/>
+    <path d="m33.1 13h-9.1v4h4.9l-0.3 4h-4.6v4h4.4l-0.3 4.5-4.1 1.4v4.2l7.9-2.6 0.7-11.5z" fill="#FFF"/>
+    <path d="m24 13v4h-8.9l-0.3-4h9.2zm-4.6 8 0.2 4h4.4v-4h-4.6zm0.4 6h-4l0.3 5.5 7.9 2.6v-4.2l-4.1-1.4-0.1-2.5z" fill="#EEE"/>
+  </svg>
+</template>
+
+<script>
+    export default {
+        name: "css_svg"
+    }
+</script>
+
+<style scoped>
+
+</style>
