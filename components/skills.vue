f7511a6e7193b16ab394cd6ce51aadf7fccd68f2
AnigalHub <anigal1997@gmail.com>
2022-04-17 22:58:54 +0300

    удален неиспользуемый import

--- components/skills.vue
+++ components/skills.vue
@@ -23,7 +23,6 @@
     import Javascript_svg from "~/components/svg_skills/javascript_svg";
     import Sass_svg from "~/components/svg_skills/sass_svg";
     import Nuxt_svg from "~/components/svg_skills/nuxt_svg";
-    import Flexbox_svg from "~/components/svg_skills/flexbox_svg";
     import Git_svg from "~/components/svg_skills/git_svg";
     import Html_svg from "~/components/svg_skills/html_svg";
     import Css_svg from "~/components/svg_skills/css_svg";
@@ -34,7 +33,7 @@
     import Express_svg from "~/components/svg_skills/express_svg";
     export default {
         name: "skills",
-      components: {Git_svg, Flexbox_svg, Nuxt_svg, Sass_svg, Javascript_svg, Vue_svg, Html_svg, NodeAndWebpack_svg,PostgreSQL_svg,
+      components: {Git_svg, Nuxt_svg, Sass_svg, Javascript_svg, Vue_svg, Html_svg, NodeAndWebpack_svg, PostgreSQL_svg,
         Typescript_svg, Bootstrap_svg, Express_svg},
       data(){
           return{
