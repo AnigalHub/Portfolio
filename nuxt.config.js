8df211ad1f7a0ec9955f2a88fd217cc8c8b7dd70
AnigalHub <anigal1997@gmail.com>
2022-04-18 22:53:43 +0300

    добавлен шрифт

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icons.png' },
+      {href: 'https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600;700&display=swap' , rel:'stylesheet'}
     ]
   },
   server:{
