448f71adb9afae78c287f22df5386c1b82adc6b8
AnigalHub <anigal1997@gmail.com>
2022-04-18 22:53:28 +0300

    добавлен пункт - Образование

--- components/navbar.vue
+++ components/navbar.vue
@@ -14,9 +14,10 @@
         <b-navbar-nav>
           <b-nav-item href="#Home">Главная</b-nav-item>
           <b-nav-item href="#About">О себе</b-nav-item>
+          <b-nav-item href="#Education">Образование</b-nav-item>
           <b-nav-item href="#Skills">Ключевые навыки</b-nav-item>
           <b-nav-item href="#Works">Мои работы</b-nav-item>
-          <b-nav-item href="#Reviews">Отзывы</b-nav-item>
+
           <b-nav-item href="#Contacts">Контакты</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
