836c12bba9528a11b02c94aa76953ca052744865
AnigalHub <anigal1997@gmail.com>
2022-04-18 22:54:38 +0300

    компонент по Образованию

---
+++ components/education.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="Education">
+    <b-container>
+      <h2>Образование</h2>
+      <b-row v-for="paragraph in AboutMeRow" :key="paragraph.name">
+        <b-col>
+          <p class="text"><b>{{paragraph.name}}</b></p>
+          <p class="text">{{paragraph.year}}</p>
+          <p class="text">Направление: {{paragraph.direction}}</p>
+          <p class="text">Кафедра: {{paragraph.department}}</p>
+        </b-col>
+        <b-col cols="2">
+          <component :is="Education_svg"></component>
+         <!-- <component :is="paragraph.svg"/> -->
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+    import Education_svg from "~/components/education_svg";
+    export default {
+      name: "education",
+      components: {Education_svg},
+      data() {
+        return {
+          Education_svg:Education_svg,
+          AboutMeRow: [
+            {
+              svg: Education_svg,
+              name: "Московский Авиационный Институт (национальный исследовательский университет) (МАИ) / Бакалавр",
+              year: "2015-2019, Москва",
+              direction:"Информатика и вычислительная техника",
+              department:"Информационные сети и технологии"
+            },
+          ]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+svg{
+  width: 300px;
+}
+</style>
