511ae0f8f80367467689ef3d7b73eb447e9a8813
AnigalHub <anigal1997@gmail.com>
2022-04-19 22:04:23 +0300

    изменена svg, текст, вывод текста

--- components/education.vue
+++ components/education.vue
@@ -3,15 +3,17 @@
     <b-container>
       <h2>Образование</h2>
       <b-row v-for="paragraph in AboutMeRow" :key="paragraph.name">
+
+
+        <b-col cols="2">
+          <component :is="Education_svg"></component>
+          <!-- <component :is="paragraph.svg"/> -->
+        </b-col>
         <b-col>
           <p class="text"><b>{{paragraph.name}}</b></p>
           <p class="text">{{paragraph.year}}</p>
           <p class="text">Направление: {{paragraph.direction}}</p>
           <p class="text">Кафедра: {{paragraph.department}}</p>
-        </b-col>
-        <b-col cols="2">
-          <component :is="Education_svg"></component>
-         <!-- <component :is="paragraph.svg"/> -->
         </b-col>
       </b-row>
     </b-container>
@@ -20,7 +22,7 @@
 
 <script>
 
-    import Education_svg from "~/components/education_svg";
+    import Education_svg from "~/components/svg_about/about_me";
     export default {
       name: "education",
       components: {Education_svg},
@@ -30,7 +32,7 @@
           AboutMeRow: [
             {
               svg: Education_svg,
-              name: "Московский Авиационный Институт (национальный исследовательский университет) (МАИ) / Бакалавр",
+              name: "Московский Авиационный Институт (национальный исследовательский университет) (МАИ)",
               year: "2015-2019, Москва",
               direction:"Информатика и вычислительная техника",
               department:"Информационные сети и технологии"
