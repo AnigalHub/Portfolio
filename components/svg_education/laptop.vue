2293832a61a4c0812704e61698af514426438829
AnigalHub <anigal1997@gmail.com>
2022-04-20 21:55:36 +0300

    добавлены @keyframes

---
+++ components/svg_education/laptop.vue
@@ -0,0 +1,243 @@
+<template>
+  <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 250 150" style="enable-background:new 0 0 250 150;" xml:space="preserve">
+    <g id="Laptop">
+      <path class="st0" d="M208.4,132l-176.8,0c-3.1,0-5.6-2.5-5.6-5.6v-0.7c0,0,0,0,0,0l188,0c0,0,0,0,0,0v0.7C214,129.5,211.5,132,208.4,132z"/>
+      <path class="st1" d="M211.2,125.8H28.8c-1.5,0-2.8-1.2-2.8-2.8v-0.7c0-1.5,1.3-2.8,2.8-2.8h182.4c1.5,0,2.8,1.2,2.8,2.8v0.7C214,124.5,212.8,125.8,211.2,125.8z"/>
+      <g>
+		    <path class="st2" d="M40.8,118.3V27.8c0-10.5,8.5-19,19-19h118.7c10.5,0,19,8.5,19,19v90.4H40.8z"/>
+        <path class="st2" d="M178.4,9.3c10.2,0,18.5,8.3,18.5,18.5v89.9H41.3V27.8c0-10.2,8.3-18.5,18.5-18.5H178.4 M178.4,8.3H59.7C49,8.3,40.3,17,40.3,27.8v90.9h157.6V27.8C197.9,17,189.2,8.3,178.4,8.3L178.4,8.3z"/>
+	    </g>
+      <ellipse id="NameLaptop" class="st3" cx="117.5" cy="122.4" rx="1.3" ry="1.9"/>
+      <ellipse id="CameraLaptop" class="st4" cx="120.1" cy="13.5" rx="0.3" ry="0.4"/>
+      <g>
+		    <rect x="47.2" y="16.5" class="st5" width="143.2" height="102.3"/>
+	    </g>
+      <g id="LinesLaptop">
+        <g id="RedLinesLaptop">
+          <g>
+				    <rect x="55.6" y="29.5" class="st6" width="6.5" height="1"/>
+            <polygon class="st6" points="62.2,29.5 55.6,29.5 55.6,30.4 62.2,30.4 62.2,29.5"/>
+			    </g>
+          <g>
+            <rect x="65.2" y="29.5" class="st6" width="38.3" height="1"/>
+            <polygon class="st6" points="103.6,29.5 65.2,29.5 65.2,30.4 103.6,30.4 103.6,29.5"/>
+          </g>
+          <g>
+            <rect x="56.2" y="68.9" class="st6" width="17.4" height="1"/>
+            <polygon class="st6" points="73.5,68.9 56.2,68.9 56.2,69.8 73.5,69.8 73.5,68.9"/>
+          </g>
+          <g>
+            <rect x="76.4" y="68.9" class="st6" width="9.4" height="1"/>
+            <polygon class="st6" points="85.8,68.9 76.4,68.9 76.4,69.8 85.8,69.8 85.8,68.9"/>
+          </g>
+          <g>
+            <rect x="146.2" y="94.8" class="st6" width="10.1" height="1"/>
+            <polygon class="st6" points="156.3,94.8 146.2,94.8 146.2,95.7 156.3,95.7 156.3,94.8"/>
+          </g>
+          <g>
+            <rect x="88.4" y="68.9" class="st6" width="5.8" height="1"/>
+            <polygon class="st6" points="94.2,68.9 88.4,68.9 88.4,69.8 94.2,69.8 94.2,68.9"/>
+          </g>
+          <g>
+            <rect x="158.9" y="45.8" class="st6" width="6.5" height="1"/>
+            <polygon class="st6" points="165.4,45.8 158.9,45.8 158.9,46.8 165.4,46.8 165.4,45.8"/>
+          </g>
+        </g>
+        <g id="BlueLinesLaptop">
+          <g>
+            <rect x="55.8" y="37.7" class="st7" width="6.5" height="1"/>
+            <polygon class="st7" points="62.3,37.7 55.8,37.7 55.8,38.6 62.3,38.6 62.3,37.7"/>
+          </g>
+          <g>
+            <rect x="65.2" y="37.7" class="st7" width="38.3" height="1"/>
+            <polygon class="st7" points="103.6,37.7 65.2,37.7 65.2,38.6 103.6,38.6 103.6,37.7"/>
+          </g>
+          <g>
+				    <rect x="107.5" y="49.2" class="st7" width="31.8" height="1"/>
+            <polygon class="st7" points="139.3,49.2 107.5,49.2 107.5,50.1 139.3,50.1 139.3,49.2"/>
+			    </g>
+          <g>
+				    <rect x="122.4" y="78.9" class="st7" width="13.7" height="1"/>
+            <polygon class="st7" points="136.1,78.9 122.4,78.9 122.4,79.9 136.1,79.9 136.1,78.9"/>
+			    </g>
+          <g>
+				    <rect x="62" y="84.2" class="st7" width="13.7" height="1"/>
+            <polygon class="st7" points="75.7,84.2 62,84.2 62,85.2 75.7,85.2 75.7,84.2"/>
+			    </g>
+          <g>
+				    <rect x="123.8" y="90" class="st7" width="13.7" height="1"/>
+            <polygon class="st7" points="137.5,90 123.8,90 123.8,90.9 137.5,90.9 137.5,90"/>
+			    </g>
+          <g>
+            <rect x="92.3" y="89.5" class="st7" width="28.2" height="1"/>
+            <polygon class="st7" points="120.5,89.5 92.3,89.5 92.3,90.5 120.5,90.5 120.5,89.5"/>
+			    </g>
+          <g>
+				    <rect x="60.5" y="104.4" class="st7" width="8.3" height="1"/>
+            <polygon class="st7" points="68.8,104.4 60.5,104.4 60.5,105.4 68.8,105.4 68.8,104.4"/>
+          </g>
+        </g>
+        <g id="WhiteLinesLaptop">
+        <g>
+          <rect x="135" y="99.6" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="160.3,99.6 135,99.6 135,100.6 160.3,100.6 160.3,99.6"/>
+        </g>
+        <g>
+          <rect x="102.8" y="99.6" class="st8" width="9.4" height="1"/>
+          <polygon class="st8" points="112.2,99.6 102.8,99.6 102.8,100.6 112.2,100.6 112.2,99.6"/>
+        </g>
+        <g>
+          <rect x="68.8" y="99.6" class="st8" width="31.1" height="1"/>
+          <polygon class="st8" points="99.9,99.6 68.8,99.6 68.8,100.6 99.9,100.6 99.9,99.6"/>
+        </g>
+        <g>
+          <rect x="84.8" y="104.4" class="st8" width="13" height="1"/>
+          <polygon class="st8" points="97.8,104.4 84.8,104.4 84.8,105.4 97.8,105.4 97.8,104.4"/>
+        </g>
+        <g>
+          <rect x="101" y="104.4" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="126.3,104.4 101,104.4 101,105.4 126.3,105.4 126.3,104.4"/>
+        </g>
+        <g>
+          <rect x="136.1" y="109.2" class="st8" width="16.6" height="1"/>
+          <polygon class="st8" points="152.7,109.2 136.1,109.2 136.1,110.2 152.7,110.2 152.7,109.2"/>
+        </g>
+        <g>
+          <rect x="106.8" y="109.2" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="132.1,109.2 106.8,109.2 106.8,110.2 132.1,110.2 132.1,109.2"/>
+        </g>
+        <g>
+          <rect x="56.6" y="109.2" class="st8" width="45.6" height="1"/>
+          <polygon class="st8" points="102.1,109.2 56.6,109.2 56.6,110.2 102.1,110.2 102.1,109.2"/>
+        </g>
+        <g>
+          <rect x="72.1" y="104.4" class="st8" width="9.4" height="1"/>
+          <polygon class="st8" points="81.5,104.4 72.1,104.4 72.1,105.4 81.5,105.4 81.5,104.4"/>
+        </g>
+        <g>
+          <rect x="56.9" y="99.6" class="st8" width="9.4" height="1"/>
+          <polygon class="st8" points="66.3,99.6 56.9,99.6 56.9,100.6 66.3,100.6 66.3,99.6"/>
+        </g>
+        <g>
+          <rect x="115.1" y="99.6" class="st8" width="16.6" height="1"/>
+          <polygon class="st8" points="131.8,99.6 115.1,99.6 115.1,100.6 131.8,100.6 131.8,99.6"/>
+        </g>
+        <g>
+          <rect x="122.4" y="94.8" class="st8" width="21" height="1"/>
+          <polygon class="st8" points="143.3,94.8 122.4,94.8 122.4,95.7 143.3,95.7 143.3,94.8"/>
+        </g>
+        <g>
+          <rect x="87.6" y="94.8" class="st8" width="30.7" height="1"/>
+          <polygon class="st8" points="118.4,94.8 87.6,94.8 87.6,95.7 118.4,95.7 118.4,94.8"/>
+        </g>
+        <g>
+          <rect x="57.3" y="94.8" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="82.6,94.8 57.3,94.8 57.3,95.7 82.6,95.7 82.6,94.8"/>
+        </g>
+        <g>
+          <rect x="57.3" y="89.5" class="st8" width="32.5" height="1"/>
+          <polygon class="st8" points="89.8,89.5 57.3,89.5 57.3,90.5 89.8,90.5 89.8,89.5"/>
+        </g>
+        <g>
+          <rect x="78.2" y="84.2" class="st8" width="45.6" height="1"/>
+          <polygon class="st8" points="123.8,84.2 78.2,84.2 78.2,85.2 123.8,85.2 123.8,84.2"/>
+        </g>
+        <g>
+          <rect x="58.4" y="78.9" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="83.7,78.9 58.4,78.9 58.4,79.9 83.7,79.9 83.7,78.9"/>
+        </g>
+        <g>
+          <rect x="86.9" y="78.9" class="st8" width="32.5" height="1"/>
+          <polygon class="st8" points="119.5,78.9 86.9,78.9 86.9,79.9 119.5,79.9 119.5,78.9"/>
+        </g>
+        <g>
+          <rect x="58.4" y="73.7" class="st8" width="45.6" height="1"/>
+          <polygon class="st8" points="103.9,73.7 58.4,73.7 58.4,74.6 103.9,74.6 103.9,73.7"/>
+        </g>
+        <g>
+          <rect x="58.7" y="49.7" class="st8" width="18.1" height="1"/>
+          <polygon class="st8" points="76.8,49.7 58.7,49.7 58.7,50.6 76.8,50.6 76.8,49.7"/>
+        </g>
+        <g>
+          <rect x="79.7" y="49.2" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="105,49.2 79.7,49.2 79.7,50.1 105,50.1 105,49.2"/>
+        </g>
+        <g>
+          <rect x="142.2" y="49.2" class="st8" width="18.1" height="1"/>
+          <polygon class="st8" points="160.3,49.2 142.2,49.2 142.2,50.1 160.3,50.1 160.3,49.2"/>
+        </g>
+        <g>
+          <rect x="140.4" y="45.8" class="st8" width="14.5" height="1"/>
+          <polygon class="st8" points="154.9,45.8 140.4,45.8 140.4,46.8 154.9,46.8 154.9,45.8"/>
+        </g>
+        <g>
+          <rect x="105.4" y="45.8" class="st8" width="30.7" height="1"/>
+          <polygon class="st8" points="136.1,45.8 105.4,45.8 105.4,46.8 136.1,46.8 136.1,45.8"/>
+        </g>
+        <g>
+          <rect x="86.6" y="45.8" class="st8" width="14.5" height="1"/>
+          <polygon class="st8" points="101,45.8 86.6,45.8 86.6,46.8 101,46.8 101,45.8"/>
+        </g>
+        <g>
+          <rect x="58.7" y="45.8" class="st8" width="25.3" height="1"/>
+          <polygon class="st8" points="84,45.8 58.7,45.8 58.7,46.8 84,46.8 84,45.8"/>
+        </g>
+        <g>
+          <rect x="81.5" y="42" class="st8" width="34.7" height="1"/>
+          <polygon class="st8" points="116.2,42 81.5,42 81.5,42.9 116.2,42.9 116.2,42"/>
+        </g>
+        <g>
+          <rect x="58.7" y="42" class="st8" width="18.1" height="1"/>
+          <polygon class="st8" points="76.8,42 58.7,42 58.7,42.9 76.8,42.9 76.8,42"/>
+        </g>
+        <g>
+          <rect x="77.5" y="33.8" class="st8" width="9.4" height="1"/>
+          <polygon class="st8" points="86.9,33.8 77.5,33.8 77.5,34.8 86.9,34.8 86.9,33.8"/>
+        </g>
+        <g>
+          <rect x="58.7" y="33.8" class="st8" width="16.6" height="1"/>
+          <polygon class="st8" points="75.4,33.8 58.7,33.8 58.7,34.8 75.4,34.8 75.4,33.8"/>
+        </g>
+		  </g>
+	  </g>
+  </g>
+</svg>
+</template>
+
+<script>
+    export default {
+        name: "about_me_svg"
+    }
+</script>
+
+<style scoped>
+  .st0{fill:#666666;stroke:#666666;stroke-miterlimit:10;}
+  .st1{fill: #afadad;stroke:#afadad;stroke-miterlimit:10;}
+  .st2{fill:#4D4D4D;}
+  .st3{fill:#FFFFFF;stroke:#4D4D4D;stroke-miterlimit:10;}
+  .st4{fill:#afadad;stroke: #afadad;stroke-miterlimit:10;}
+  .st5{fill:#003052;}
+  .st6{fill:#F7931E;}
+  .st7{fill:#29ABE2;}
+  .st8{fill:#F2F2F2;}
+  #WhiteLinesLaptop{animation: whiteLinesLaptop 3s infinite linear;}
+  @keyframes whiteLinesLaptop {
+    0% { stroke: #cdc4c4;}
+    50% {stroke:#cdc4c4;}
+    100% {stroke: #29ABE2;}
+  }
+  #BlueLinesLaptop{animation: blueLinesLaptop 3s infinite linear;}
+  @keyframes blueLinesLaptop {
+    0% { stroke: #29ABE2;}
+    50% {stroke: #29ABE2;}
+    100% {stroke: #F2F2F2;}
+  }
+  #RedLinesLaptop{animation: redLinesLaptop 3s infinite linear;}
+  @keyframes redLinesLaptop {
+    0% { stroke: #e88f0c;}
+    50% {stroke:  #F7931E;}
+    100% {stroke: #975d08;}
+  }
+
+</style>
