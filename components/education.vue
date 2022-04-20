a0015dc369fe3488a2f85c003d71a0b33f2b00f3
AnigalHub <anigal1997@gmail.com>
2022-04-20 21:57:53 +0300

    массив заменен на объект

--- components/education.vue
+++ components/education.vue
@@ -2,18 +2,15 @@
   <div id="Education">
     <b-container>
       <h2>Образование</h2>
-      <b-row v-for="paragraph in AboutMeRow" :key="paragraph.name">
-
-
+      <b-row>
         <b-col cols="2">
           <component :is="Education_svg"></component>
-          <!-- <component :is="paragraph.svg"/> -->
         </b-col>
-        <b-col>
-          <p class="text"><b>{{paragraph.name}}</b></p>
-          <p class="text">{{paragraph.year}}</p>
-          <p class="text">Направление: {{paragraph.direction}}</p>
-          <p class="text">Кафедра: {{paragraph.department}}</p>
+        <b-col class="text">
+          <p><b>{{Education.name}}</b></p>
+          <p>{{Education.year}}</p>
+          <p>Направление: {{Education.direction}}</p>
+          <p>Кафедра: {{Education.department}}</p>
         </b-col>
       </b-row>
     </b-container>
@@ -21,30 +18,25 @@
 </template>
 
 <script>
-
-    import Education_svg from "~/components/svg_about/about_me";
+    import Education_svg from "~/components/svg_education/laptop";
     export default {
       name: "education",
       components: {Education_svg},
       data() {
         return {
           Education_svg:Education_svg,
-          AboutMeRow: [
-            {
-              svg: Education_svg,
-              name: "Московский Авиационный Институт (национальный исследовательский университет) (МАИ)",
-              year: "2015-2019, Москва",
-              direction:"Информатика и вычислительная техника",
-              department:"Информационные сети и технологии"
-            },
-          ]
+          Education: {
+            svg: Education_svg,
+            name: "Московский Авиационный Институт (национальный исследовательский университет) (МАИ)",
+            year: "2015-2019, Москва",
+            direction:"Информатика и вычислительная техника",
+            department:"Информационные сети и технологии"
+          }
         }
       }
     }
 </script>
 
 <style scoped>
-svg{
-  width: 300px;
-}
+
 </style>
