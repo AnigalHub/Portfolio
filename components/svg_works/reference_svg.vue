de6784a0693df9490b748473fe8b118b5ba6f96e
AnigalHub <anigal1997@gmail.com>
2022-04-21 23:05:17 +0300

    svg на ссылку с кодом на гитхаб

---
+++ components/svg_works/reference_svg.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 70 70" style="enable-background:new 0 0 70 70;" xml:space="preserve">
+    <g>
+	<g>
+		<line class="st1" x1="22.7" y1="20.8" x2="13.2" y2="33.6"/>
+	</g>
+      <g>
+		<line class="st1" x1="13" y1="33" x2="23.1" y2="46.1"/>
+	</g>
+</g>
+    <line class="st1" x1="41" y1="15.5" x2="26.8" y2="51.8"/>
+    <g>
+	<g>
+		<line class="st1" x1="45.2" y1="22" x2="54.7" y2="34.8"/>
+	</g>
+      <g>
+		<line class="st1" x1="55" y1="34" x2="44.9" y2="47.3"/>
+	</g>
+</g>
+</svg>
+
+</template>
+
+<script>
+    export default {
+        name: "reference_svg"
+    }
+</script>
+
+<style scoped>
+  svg{
+    margin-left: -5px;
+  }
+  .st1{fill:none;stroke:#000;stroke-width:2;stroke-miterlimit:10;}
+</style>
