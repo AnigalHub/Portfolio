817464dd8df1e0d48b0f6a5044a3d9c12262a8b1
AnigalHub <anigal1997@gmail.com>
2022-04-22 23:25:09 +0300

    изменено название картинки

--- components/home.vue
+++ components/home.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="Home">
       <div class="container">
-        <img src="../static/6.jpg">
+        <img src="../static/me.jpg">
         <p class="name" >Игнатова Анна</p>
         <p class="front">Junior Frontend-разработчик</p>
       </div>
