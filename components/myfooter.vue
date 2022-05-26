822f9e2c4b2069f3bf88bf86b7adf2fdb82ba5d5
AnigalHub <anigal1997@gmail.com>
2022-05-26 21:24:33 +0300

    удален text

--- components/myfooter.vue
+++ components/myfooter.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div id="Myfooter">
     <b-container >
       <div class="flex-container text">
@@ -23,33 +22,29 @@
     import Telephone_svg from "~/components/svg_contacts/telephone_svg";
     import Email_svg from "~/components/svg_contacts/email_svg";
     export default {
-        name: "myfooter",
-        components: {Logo_svg, Github_svg, Telegram_svg, Telephone_svg, Email_svg},
+      name: "myfooter",
+      components: {Logo_svg, Github_svg, Telegram_svg, Telephone_svg, Email_svg},
       data(){
         return{
           ContactsRow:[
             {
               svg:Telephone_svg,
               href:'tel:' + '+7 (903) 151-39-31',
-              text:'+7 (903) 151-39-31',
               target:'_self'
             },
             {
               svg:Email_svg,
-              href:'mailto:' + 'rezvetz@gmail.com',
-              text:'annaignatova97@bk.ru',
+              href:'mailto:' + 'annaignatova97@bk.ru',
               target:'_self'
             },
             {
               svg:Github_svg,
               href:'https://github.com/AnigalHub',
-              text:'github.com/AnigalHub',
               target:'_blank'
             },
             {
               svg:Telegram_svg,
               href:'https://t.me/annaignatova97',
-              text:'@annaignatova97',
               target:'_blank'
             },
           ]
