3802764904908b480e56b235aa269fe494ce6c25
AnigalHub <anigal1997@gmail.com>
2022-05-28 01:40:28 +0300

    в массив добавлен пункт about

--- components/works.vue
+++ components/works.vue
@@ -7,9 +7,11 @@
           <img :src="work.src" :alt="work.alt" @click="showModal(work)"/>
           <div class="work">
             <div class="name_works">
-              <p class="name">{{work.name_company}}</p>
+
               <p class="text_works">{{work.technology}}</p>
             </div>
+            <p class="name">{{work.name_company}}</p>
+            <p class="text_about">{{work.about}}</p>
           </div>
         </div>
       </div>
@@ -34,99 +36,110 @@
                 main_src:'./prom.jpg',
                 src:'./home_prom.jpg',
                 alt:'Промэлектронсервис',
-                name_company:'Сайт «Промэлектронсервис»',
-                technology:'HTML, CSS, Bootstrap, PHP.',
+                name_company:'«Промэлектронсервис»',
+                technology:'HTML & CSS & Bootstrap',
+                about:'Сайт сертификационных испытаний и комплексной поставки электронных компонентов'
               },
               {
                 main_src:'./gts.jpg',
                 src:'./home_gts.png',
                 alt:'Гостехносервис',
-                name_company:'Сайт «Гостехносервис»',
-                technology:'Vue.js, BootstrapVue, Sass.',
+                name_company:'«Гостехносервис»',
+                technology:'Vue.js',
+                about:'Сайт производителя и поставщика ИТ‒оборудования для государственных и бизнес-структур'
               },
               {
                 main_src:'./priamyeruki.jpg',
                 src:'./home_priamyeruki.jpg',
                 alt:'Прямые_руки',
-                name_company:'Сайт «Аваконт»',
-                technology:'Vue.js, Sass, Express.js.',
+                name_company:'«Аваконт»',
+                technology:'Vue.js',
+                about:'Сайт изготовления разнообразного декора, рекламных вывесок, сувенирной продукции, реставрации, брендирования'
               },
               {
                 main_src:'./avacont.jpg',
                 src:'./home_avacont.jpg',
                 alt:'Аваконт',
-                name_company:'Сайт «Аваконт»',
-                technology:'Vue.js, JavaScript, HTML, Sass, Express.js.',
+                name_company:'«Аваконт»',
+                technology:'Vue.js',
+                about: 'Сайт изготовления накладок из дерева под розетки, выключатели, радиаторы, светильники и карнизы'
               },
               {
                 main_src:'./valencia.png',
                 src:'./home_valencia.png',
                 alt:'Валенсия',
-                name_company:'Сайт «Валенсия»',
-                technology:'Nuxt.js, BootstrapVue, HTML, Sass.',
+                name_company:'«Валенсия»',
+                technology:'Nuxt.js',
+                about:'Сайт частного охранного предприятия, обеспечивающего безопасность на всей территории во всех субъектах РФ.'
               },
               {
                 main_src:'./pixelperfect.png',
                 src:'/home_pixelperfect.png',
                 alt:'pixelperfect',
-                name_company:'Сайт по Макету',
-                technology:'Vue.js, BootstrapVue, HTML, Sass.',
+                name_company:'Startup',
+                technology:'Vue.js',
+                about: 'Шаблон с попиксельной точностью, выполненный с помощью подхода Pixel Perfect'
               },
-              {
-                main_src:'./main_api.png',
-                src:'/api.png',
-                alt:'api',
-                name_company:'Проект “Аптечка”',
-                technology:'Node.js, Express.js, Vue.js, BootstrapVue, PostgreSQL.',
-              },
-              {
-                main_src:'./main_wishlist.jpg',
-                src:'/wishlist.png',
-                alt:'wishlist',
-                name_company:'Проект “Список Желаний”',
-                technology:'Node.js, Express.js, Vue.js, BootstrapVue, PostgreSQL.',
-              },
+              // {
+              //   main_src:'./main_api.png',
+              //   src:'/api.png',
+              //   alt:'api',
+              //   name_company:'Проект “Аптечка”',
+              //   technology:'Node.js, Express.js, Vue.js, BootstrapVue, PostgreSQL.',
+              // },
+              // {
+              //   main_src:'./main_wishlist.jpg',
+              //   src:'/wishlist.png',
+              //   alt:'wishlist',
+              //   name_company:'Проект “Список Желаний”',
+              //   technology:'Node.js, Express.js, Vue.js, BootstrapVue, PostgreSQL.',
+              // },
               {
                 main_src:'./main_training.jpg',
                 src:'./training.png',
                 alt:'Математический тренажёр',
-                name_company:'Игра "Математический тренажёр"',
-                technology:'JavaScript, HTML, CSS, Webpack.',
+                name_company:'«Математический тренажёр»',
+                technology:'JavaScript',
+                about: 'Игра для малышей на сложение, вычитание, деление и умножение с таймером'
               },
               {
                 main_src:'./main_snake.jpg',
                 src:'./snake.png',
                 alt:'Игра "Змейка"',
-                name_company:'Игра "Змейка"',
-                technology:'JavaScript, Canvas, Webpack.',
+                name_company:'«Змейка»',
+                technology:'JavaScript',
+                about:'Классическая игра передвижения змейки за едой на игровом поле с помощью управления клавишами'
               },
               {
                 main_src:'./main_svgGame.jpg',
                 src:'./home_svgGame.png',
                 alt:'alphabet',
-                name_company:'Игра "Изучение Алфавита"',
-                technology:'Vue.js, JavaScript, Drag and Drop, Sass. ',
+                name_company:'"Алфавит"',
+                technology:'Vue.js',
+                about:'Игры на знание английского алфавита: изучение алфавита, собери пару и конструктор слов'
               },
-              {
-                main_src:'./main.jpg',
-                src:'/parser.jpg',
-                alt:'parser',
-                name_company:'Парсинг сайта ',
-                technology:'Node.js',
-              },
+              // {
+              //   main_src:'./main.jpg',
+              //   src:'/parser.jpg',
+              //   alt:'parser',
+              //   name_company:'Парсинг сайта ',
+              //   technology:'Node.js',
+              // },
               {
                 main_src:'./main_TestTask.png',
                 src:'/TestTask.png',
                 alt:'TestTask',
-                name_company:'Страница продажи товаров',
-                technology:'Nuxt.js, Vuex, Sass.',
+                name_company:'Продажа товаров',
+                technology:'Nuxt.js',
+                about:'Целевая страница с формой на добавление товара в список и сортировкой товара в списке'
               },
               {
                 main_src:'./main_market.png',
                 src:'/market.png',
                 alt:'market',
-                name_company:'Каталог с корзиной и сортировкой',
-                technology:'Nuxt.js, Vuex, Sass.',
+                name_company:'Каталог товаров',
+                technology:'Nuxt.js',
+                about:'Целевая страница с возможностью сортировки товаров, отправки заявки и удалении из корзины товара'
               },
               /*   {
                       main_src:'',
@@ -141,8 +154,9 @@
                 main_src:'./main_TestBis.jpg',
                 src:'/TestBis.png',
                 alt:'TestBis',
-                name_company:'Прототип банковского приложения',
-                technology:'Vue.js (Vuex, VueRouter, BootstrapVue).',
+                name_company:'Банковское приложение',
+                technology:'Vue.js',
+                about:'Прототип банковского приложения с возможностью отображения таблиц, изменения и удаления данных из них'
               },
             ],
           }
