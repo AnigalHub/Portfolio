442d3c2bbb8e42a494155c8890cbef876726ed34
AnigalHub <anigal1997@gmail.com>
2022-05-28 16:55:01 +0300

    удалены Skills

--- components/about.vue
+++ components/about.vue
@@ -6,23 +6,19 @@
         <b-col cols="4">
           <img src="../static/me.jpg">
           <p class="name">Игнатова Анна</p>
-          <div class="flex-container">
-            <div v-for="(skill,index) in Skills" :key="index">
-              <div class="text number">{{skill.svg}}</div>
-              <div class="text">{{skill.skills_name}}</div>
+            <div class="projects">
+              <div class="text number">20+</div>
+              <div class="text">Завершенных проектов</div>
             </div>
-          </div>
         </b-col>
         <b-col>
-
-            <ul>
-              <li class="text" v-for="(education,index) in About" :key="index">
-                <p><b>{{education.name}}<span> {{education.year}}</span></b></p>
-                <p>{{education.direction}}</p>
-                <p>{{education.department}}</p>
-              </li>
-            </ul>
-        
+          <ul>
+            <li class="text" v-for="(education,index) in About" :key="index">
+              <p><b>{{education.name}}<span> {{education.year}}</span></b></p>
+              <p>{{education.direction}}</p>
+              <p>{{education.department}}</p>
+            </li>
+          </ul>
         </b-col>
       </b-row>
     </b-container>
@@ -34,10 +30,6 @@
       name: "about",
       data() {
         return {
-          Skills:[
-            // {svg:'5+', skills_name: 'Лет изучения Web-разработки',},
-            {svg:'20+', skills_name: 'Завершенных проектов',},
-          ],
           About:[
             {
               name: "Московский Авиационный Институт (МАИ)",
