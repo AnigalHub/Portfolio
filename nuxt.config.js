4485546eca8b50b4321042faacecdb3feb3068f7
AnigalHub <anigal1997@gmail.com>
2022-05-29 21:57:01 +0300

    изменен title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Портфолио Игнатова Анна',
+    title: 'Игнатова Анна | Портфолио',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
